dda3816795b6110046c20ad25928a67e2e1368aa
Ganief <mgcarloo6@gmail.com>
2020-06-22 20:19:16 +0200

    update

--- .history/client/src/context/contact/ContactState_20200622192402.js
+++ .history/client/src/context/contact/ContactState_20200622192402.js
@@ -66,47 +66,8 @@ const ContactState = (props) => {
   };
 
   //   Delete Contact
-  const deleteContact = async (id) => {
-    try {
-      await axios.delete(`/api/contacts/${id}`);
-
-      dispatch({
-        type: DELETE_CONTACT,
-        payload: id,
-      });
-    } catch (err) {
-      dispatch({
-        type: CONTACT_ERROR,
-        payload: err.response.msg,
-      });
-    }
-  };
-
-  //   Update Contact
-  const UpdateContact = async (contact) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    try {
-      const res = await axios.put(
-        `/api/contacts/${contact._id}`,
-        contact,
-        config
-      );
-
-      dispatch({
-        type: UPDATE_CONTACT,
-        payload: res.data,
-      });
-    } catch (err) {
-      dispatch({
-        type: CONTACT_ERROR,
-        payload: err.response.msg,
-      });
-    }
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
   };
 
   // Clear Contacts
@@ -122,6 +83,11 @@ const ContactState = (props) => {
   //   Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
+  };
+
+  //   Update Contact
+  const UpdateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //   Filter Contact
